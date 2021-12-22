51bc70b8c599a04f65c2c40a1503f2cf7c575e0a
MAXvaLL <maxvall@ya.ru>
2021-12-22 19:19:12 +0300

    Convert script: fix 2 stage processing (v3)

--- scripts/convert/conv.py
+++ scripts/convert/conv.py
@@ -22,35 +22,29 @@ for ln in f_in :
     txt += ln
 
 result = u""
-for ln in txt :
+for ln in txt.split('\n'):
+    ln += '\n'
     # print(ln,end="",file=f_log)
 
     #RED MARKUP
-    # ln = re.sub(r'^[ \t]*([ПДЧН])[ \t]+', r'**\1** ', ln, flags=re.MULTILINE) #П/Д/Н/Ч
-    # ln = re.sub(r'((?:Припев|Слава,[ \t]*и ныне|Слава|И[ \t]+ныне)\S*)', r'**\1**', ln) #Припев/Слава/И ныне/Слава, и ныне
-    # ln = re.sub(r'\(или(\S*)(.*?)\)', r'**[или\1**\2**]**', ln) #(или:...)/(или ...) => **[или:**...**]**
-    # ln = re.sub(r'\(или(\S*)(.*?)\)', r'**(или\1**\2**)**', ln) #(или:...)/(или ...) => **(или:**...**)**
-
-    # ln = re.sub(r'^((?:|[^#].*))\(([^)]*[^*])\)', r'\1**(\2)**', ln, flags=re.MULTILINE) #(...) [RESTRICT IN HEADERS] => **()**
-    # ln = re.sub(r'^([^#].*)\((.*?)\)(?!\*)', r'\1**(\2)**', ln, flags=re.MULTILINE) #(...) [RESTRICT IN HEADERS] => **()**
-    ln = re.sub(r'^(?:[^#].*?)\((.*?)\)(?!\*)', r'**(\1)**', ln, flags=re.MULTILINE) #(...) [RESTRICT IN HEADERS] => **()**
-    # ln = re.sub(r'\((.*?)\)(?!\*)', r'**(\1)**', ln) #(...) => **()** [RESTRICT DOUBLE MARKUP]
-    # ln = re.sub(r'\((.*?)\)', r'**(\1)**', ln) #(...) => **()**
-
-    # ln = re.sub(r'//', r'**//**', ln) #//
-    # ln = re.sub(r'/(\s+)', r'**/**\1', ln) # /_
-    # ln = re.sub(r'\*\*\*\*', r'', ln) #remove gluing "****"
+    if ln[0] != '#': #[RESTRICT IN HEADERS]
+        # ln = re.sub(r'\(или(\S*)(.*?)\)', r'**[или\1**\2**]**', ln) #(или:...)/(или ...) => **[или:**...**]**
+        ln = re.sub(r'\(или(\S*)(.*?)\)', r'**(или\1**\2**)**', ln) #(или:...)/(или ...) => **(или:**...**)**
+        ln = re.sub(r'(?<!\*)\((.*?)\)(?!\*)', r'**(\1)**', ln) #(...) => **()** [RESTRICT DOUBLE MARKUP]
+    ln = re.sub(r'^[ \t]*([ПДЧН])[ \t]+', r'**\1** ', ln) #П/Д/Н/Ч
+    ln = re.sub(r'((?:Припев|Слава,[ \t]*и ныне|Слава|И[ \t]+ныне)\S*)', r'**\1**', ln) #Припев/Слава/И ныне/Слава, и ныне
+    ln = re.sub(r'//', r'**//**', ln) #//
+    ln = re.sub(r'/(\s+)', r'**/**\1', ln) # /_
+    ln = re.sub(r'\*\*\*\*', r'', ln) #remove gluing "****"
 
     #SMALLTEXT
-    # /-ую /-у /-ю /-ей /-ы ... /ёе
-    ln = re.sub(r'/-([а-яё]+)', r'/`-\1`', ln)
-    ln = re.sub(r'/(?:ёе)', r'/`\1`', ln)
+    ln = re.sub(r'/-([а-яё]+)', r'/`-\1`', ln) # /-ую /-у /-ю /-ей /-ы ...
+    ln = re.sub(r'/(?:ёе)', r'/`\1`', ln) # /ёе
 
     #LINE NUMBERING
-    ln = re.sub(r'^[ \t]*(\d)[ \t]+', r'~~\1~~ ', ln, flags=re.MULTILINE)
-    
+    ln = re.sub(r'^[ \t]*(\d)[ \t]+', r'~~\1~~ ', ln)
+
     # ln = re.sub(r'', r'', ln)
-    
     result += ln
 
 f_out.write(result)
